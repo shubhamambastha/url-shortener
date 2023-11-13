c2450a36f409f48412169ce483aef804866d0cb4
Shubham Ambastha <shubhams.saurav@gmail.com>
2023-11-13 23:31:14 +0530

    lint fix

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -14,11 +14,11 @@ const createToken = (user: User): TokenData => {
   const expiresIn: number = 60 * 60;
 
   return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
-}
+};
 
 const createCookie = (tokenData: TokenData): string => {
   return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
-}
+};
 
 @Service()
 @EntityRepository()
@@ -37,7 +37,7 @@ export class AuthService extends Repository<UserEntity> {
     if (!findUser) throw new HttpException(409, `This email ${userData.email} was not found`);
 
     const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
-    if (!isPasswordMatching) throw new HttpException(409, "Password not matching");
+    if (!isPasswordMatching) throw new HttpException(409, 'Password not matching');
 
     const tokenData = createToken(findUser);
     const cookie = createCookie(tokenData);
