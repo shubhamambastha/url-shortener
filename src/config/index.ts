1d3d811c81c81f1b0a0d04942b2b5ab69fca4fa9
Shubham Ambastha <shubhams.saurav@gmail.com>
2023-11-13 22:08:48 +0530

    initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DATABASE } = process.env;
