c8db7e02ac4a7e4ddd1edee3cd1a116dc6f7f6ad
Shubham Ambastha <shubhams.saurav@gmail.com>
2023-11-15 02:18:14 +0530

    added route to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
+import { UrlRoute } from '@routes/urls.route';
 import { ValidateEnv } from '@utils/validateEnv';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new UserRoute()]);
+const app = new App([new AuthRoute(), new UserRoute(), new UrlRoute()]);
 
 app.listen();
