3b2c8161f7da5602d17d8f693f34766bdc4a1ceb
Shubham Ambastha <shubhams.saurav@gmail.com>
2023-11-15 02:16:50 +0530

    update url entity

--- src/entities/urls.entity.ts
+++ src/entities/urls.entity.ts
@@ -1,11 +1,11 @@
 import { IsNotEmpty } from 'class-validator';
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { Urls } from '@interfaces/urls.interface';
 import { UserEntity } from './users.entity';
 
 @Entity({ name: 'urls' })
 export class UrlEntity extends BaseEntity implements Urls {
-  @PrimaryGeneratedColumn({ type: 'bigint' })
+  @PrimaryColumn({ type: 'bigint' })
   id: number;
 
   @Column({ name: 'long_url', length: 1024, nullable: false })
